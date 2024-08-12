1cc817191d68352d676a1067607c5c45b2007e0b
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-08-12 23:51:05 +0000

    Filter places by Amenity

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    let selectedAmenities = {};
+
+    // Listen for changes on checkbox inputs
+    $('.amenities input[type="checkbox"]').change(function () {
+        const amenityId = $(this).attr('data-id');
+        const amenityName = $(this).attr('data-name');
+
+        if (this.checked) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+
+        const amenitiesList = Object.values(selectedAmenities).join(', ');
+        $('.amenities h4').text(amenitiesList);
+    });
+
+    // Listen for button click to filter places
+    $('button').click(function () {
+        const amenities = Object.keys(selectedAmenities);
+
+        $.ajax({
+            url: 'http://0.0.0.0:5001/api/v1/places_search/',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ amenities: amenities }),
+            success: function (data) {
+                $('section.places').empty();
+
+                data.forEach(place => {
+                    const article = `
+                        <article>
+                            <div class="title_box">
+                                <h2>${place.name}</h2>
+                                <div class="price_by_night">$${place.price_by_night}</div>
+                            </div>
+                            <div class="information">
+                                <div class="max_guest">${place.max_guest} Guests</div>
+                                <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                                <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                            </div>
+                            <div class="description">
+                                ${place.description}
+                            </div>
+                        </article>`;
+                    $('section.places').append(article);
+                });
+            }
+        });
+    });
+});
